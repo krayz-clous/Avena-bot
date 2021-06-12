15fadfc9dcb0ea5bb1c123cbe97077ceeb414808
conghal334 <72975273+conghal334@users.noreply.github.com>
2021-06-12 23:02:56 +0700

    Initial commit

---
+++ events/joinMessage.js
@@ -0,0 +1,23 @@
+const client = require('../index')
+const { MessageEmbed } = require('discord.js');
+client.on('guildCreate', (guild) => {
+    let channelToSend;
+
+    guild.channels.cache.forEach((channel) => {
+        if (
+            channel.type === 'text' && 
+            !channelToSend && 
+            channel.permissionsFor(guild.me).has('SEND_MESSAGES')
+            ) channelToSend = channel;
+    });
+
+    if(!channelToSend) return;
+
+    channelToSend.send(
+        new MessageEmbed()
+        .setAuthor(guild.name, guild.iconURL({ dynamic: true }))
+        .setDescription("<a:loading:853295921211113472> Thanks you for inviting me! My prefix is `;` I will be more than happy to help out this server! And don't forget to give me permission! If you need anymore help regarding the bot join the support server! https://discord.gg/PRdKamPre7")
+        .setTimestamp()
+        .setFooter("Have fun!")
+    )
+});
---
+++ shopItems.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        item: 'Sword',
+        price: 5000
+    },
+    {
+        item: 'Fruits',
+        price: 100
+    },
+    {
+        item: "Gold",
+        price: 10000
+    },
+    {
+        item: "Diamond",
+        price: 20000
+    }
+]
