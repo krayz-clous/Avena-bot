25969288cdcf95f190b06935c1e76861ce317995
conghal334 <72975273+conghal334@users.noreply.github.com>
2021-06-13 16:48:22 +0700

    fixed vote cmd

--- commands/Bot/vote.js
+++ commands/Bot/vote.js
@@ -13,7 +13,7 @@ module.exports = {
           new MessageEmbed()
           .setTitle('Vote for Avena!')
           .addField(`Discord Bots List`, `[Click Here](https://disbotlist.xyz/bot/853225627926003732/vote)`)
-          .addField(`Void Bots` `Coming Soon`)
+          .addField(`Void Bots`, `Coming Soon`)
           .setColor('RANDOM')
         )
     },
