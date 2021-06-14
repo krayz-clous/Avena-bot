5d5579b55b8b228afa2859b3b3cebef8de45d08b
conghal334 <72975273+conghal334@users.noreply.github.com>
2021-06-14 12:54:38 +0700

    update backup

--- commands/backup/backup.js
+++ commands/backup/backup.js
@@ -9,12 +9,13 @@ module.exports = {
   name: "backup",
   aliases: [""],
   run: async (client, message, args) => {
+    const cmd = args.shift().toLowerCase();
+    let word = args.join(" ");
     let info = "ℹ️"; 
     let waiting = "⌛"; 
     let green = "✅"; 
     let error = "❌";
     let warning = "⚠️";
-    const cmd = args.shift().toLowerCase();
     let guildsonlyEmbed = new MessageEmbed()
       .setTitle(`${error} Error`)
       .setDescription(
@@ -43,7 +44,7 @@ module.exports = {
 
            let id = makeid(16)
 
-               const channels = message.guild.channels.sort(function (a, b) { return a.position - b.position; }).array().map(c => {
+               const channels = message.guild.channels.cache.sort(function (a, b) { return a.position - b.position; }).array().map(c => {
                const channel = {
                    type: c.type,
                    name: c.name,
@@ -55,7 +56,7 @@ module.exports = {
 
  
        
-               const roles = message.guild.roles.filter(r => r.name !== "@everyone").sort(function (a, b) { return a.position - b.position; }).array().map(r => {
+               const roles = message.guild.roles.cache.filter(r => r.name !== "@everyone").sort(function (a, b) { return a.position - b.position; }).array().map(r => {
                const role = {
                    name: r.name,
                    color: r.color,
@@ -82,23 +83,91 @@ module.exports = {
            let result = new MessageEmbed()
            .setTitle(`${info}  Info`)
            .setDescription(`Created backup of **${message.guild.name}** with the Backup id \`${id}\``)
-           .addField("Usage", `\`\`\`!backup load ${id}\`\`\`
-\`\`\`!backup info ${id}\`\`\``)
+           .addField("Usage", `\`\`\`${prefix}backup load ${id}\`\`\`
+\`\`\`${prefix}backup info ${id}\`\`\``)
            .setColor(color)
 
        message.author.send(result)
 
        let resultPublic = new MessageEmbed()
-       .setTitle(`${green}  Voila!`)
+       .setTitle(`${green} Created!`)
        .setDescription(`Created backup of **${message.guild.name}** with the Backup id \`${id}\``)
-       .addField("Usage", `\`\`\`!backup load ${id}\`\`\`
-\`\`\`!backup info ${id}\`\`\``)
+       .addField("Usage", `\`\`\`${prefix}backup load ${id}\`\`\`
+\`\`\`${prefix}backup info ${id}\`\`\``)
        .setColor(color)
 
    m.edit(resultPublic)
        
          })
        }
+       if(cmd === "delete") {
+        let code = word;
+        let errorEmbed = new MessageEmbed()
+        .setTitle(`${error}  Error`)
+        .setDescription(`You forgot to define the argument backup_id or incorrect. Use ${prefix}help backup load for more information.`)
+        .setColor(color)
+        if(!code) return message.channel.send(errorEmbed)
+
+        let cantfindbackup = new MessageEmbed()
+        .setTitle(`${error}  Error`)
+        .setTitle(`You have no backup with the id ${code}.`)
+        .setDescription(`Need Help? Join Our Support Server [Click Here](https://discord.gg/cFk7VsfkZS)`)
+        .setColor(color)
+        if(!backups[message.author.id][code]) return message.channel.send(cantfindbackup)
+
+        delete backups[message.author.id][code];
+        save();
+
+        let deletedsuc = new MessageEmbed()
+            .setTitle(`${green} Deleted!`)
+            .setDescription(`Successfully **deleted backup**.`)
+            .setColor(color)
+            message.channel.send(deletedsuc)
+
+    }
+    if(cmd === "load") {
+      let error = "❌"
+      let code = word;
+      let errorEmbed = new MessageEmbed()
+      .setTitle(`${error}  Error`)
+      .setDescription(`You forgot to define the argument backup_id. Use ${prefix}help backup load for more information.`)
+      if(!code) return message.channel.send(errorEmbed)
+      let cantfindbackup = new MessageEmbed()
+      .setTitle(`${error}  Error`)
+      .setTitle(`You have no backup with the id ${code}.`)
+      .setDescription("[Support](https://discord.gg/cFk7VsfkZS)")
+      .setColor(color)
+      if(!backups[message.author.id][code]) return message.channel.send(cantfindbackup)
+      
+      message.guild.channels.cache.forEach(channel => {
+          channel.delete('For Loading A Backup')
+      })
+
+      message.guild.roles.cache.filter(role => role.members.every(member => !member.user.bot)).forEach(role => {
+          role.delete('For Loading A Backup')
+      })
+      await backups[message.author.id][code].roles.forEach(async function (role) {
+                message.guild.roles.create({ name: role.name, color: role.color, permissions: role.permissions, hoist: role.hoist, mentionable: role.mentionable, position: role.position }).then(role => {
+                  role.setPosition(role.position)
+              })
+      
+      });
+
+     await backups[message.author.id][code].channels.filter(c => c.type === "category").forEach(async function (ch)  {
+              message.guild.channels.create(ch.name, ch.type, ch.permissionOverwrites)
+      }); 
+
+     await backups[message.author.id][code].channels.filter(c => c.type !== "category").forEach(async function(ch) {
+              message.guild.channels.create(ch.name, ch.type, ch.permissionOverwrites).then(c => {
+                  const parent = message.guild.channels.cache.filter(c => c.type === 'category').find(c => c.name === ch.parent);
+                  ch.parent ? c.setParent(parent) : '';
+              });
+      });
+       message.guild.setName(backups[message.author.id][code].name)
+       message.guild.setIcon(backups[message.author.id][code].icon)
+
+  }
+
     if (!args[0]) {
       const embed = new MessageEmbed()
         .setTitle(
