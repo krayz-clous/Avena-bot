87ac2477e287d7f21006f01745260beae2a0915b
conghal334 <72975273+conghal334@users.noreply.github.com>
2021-06-15 13:05:26 +0700

    fix time

--- commands/Utility/speak.js
+++ commands/Utility/speak.js
@@ -19,7 +19,7 @@ module.exports = {
             lang: lang,
             slow: false,
             host: "https://translate.google.com",
-            timeout: 30000,
+            timeout: 10000,
         }); 
         try {
             voiceChannel.join().then(connection => {
