2e3f2ac987f7ef2b7d6d4fb0767e87edcca97dee
conghal334 <72975273+conghal334@users.noreply.github.com>
2021-06-15 15:52:51 +0700

    fixed

--- command/pong.js
+++ command/pong.js
@@ -1,17 +1,15 @@
 const { MessageEmbed } = require("discord.js")
 const client = require("../index")
-const { color } = require("../config.json")
 
 module.exports = {
     slash: true,
     testOnly: true,
-    description: "Invite your bot to your server",
+    description: "TEST",
     callback: ({  }) => {
         let embed = new MessageEmbed()
-        .setTitle("\*\*Invite me to your server now\*\*")
-        .addField(`[Click Here](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands)`)
-        .setColor(color)
-        .setTimestamp()
+        .setTitle("Pong!")
+        .setColor("RANDOM")
+        .setDescription(`Websocket Ping ${client.ws.ping}`)
 
         return embed
     }
