61f30b2fb8e887a0fe97cd453ff6d8639fbfa2a6
conghal334 <72975273+conghal334@users.noreply.github.com>
2021-06-19 21:57:24 +0700

    update

--- commands/moderation/nick.js
+++ commands/moderation/nick.js
@@ -1,32 +1,43 @@
-const { Client, Message, MessageEmbed } = require("discord.js");
+const { MessageEmbed } = require("discord.js")
+const Discord = require("discord.js")
+const limit = require("@codedipper/random-code")
+        module.exports = {
+            name: 'nickname',
+            aliases: ["nn", "nname"],
+            description: "Moderate nickname",
+            usage: "<prefix>nickname <user> <new nickname> ",
+            /**
+           * @param {Client} client
+           * @param {Message} message
+           * @param {String[]} args
+           */
+          run: async (client, message, args ) => {
+        if (!message.member.hasPermission('MANAGE_NICKNAMES')) return message.channel.send(
+          new MessageEmbed()
+          .setDescription(":x: You do not have permission to use this command")
+          .setFooter(`Requested by ${message.author.tag}`,message.author.displayAvatarURL({dynamic: true})))
+        
+       if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send(
+         new MessageEmbed()
+         .setDescription(":x: I do not have permission to use this command")
+         .setFooter(`Requested by ${message.author.tag}`,message.author.displayAvatarURL({dynamic: true}))
+       )
+              let member = message.mentions.members.first()
+              if (!member) return message.channel.send(
+                new MessageEmbed()
+                .setDescription(":x: Please specify a member")
+                .setFooter(`Requested by ${message.author.tag}`,message.author.displayAvatarURL({dynamic: true}))
+              )
 
-module.exports = {
-  name: "nickname",
-  /**
-   * @param {Client} client
-   * @param {Message} message
-   * @param {String[]} args
-   */
-  run: async (client, message, args) => {
-      if(!message.member.hasPermission('ADMINISTRATOR')) return;
-    const member = message.mentions.members.first();
+              if(message.member.roles.highest.position <= member.roles.highest.position) return message.channel.send("I can't do this, try to put my role higher")
+let random = limit(4);
+      member.setNickname(`Moderated Nickname ${random}`)
 
-    if (!member) return message.reply("Please specify a member of whom you want to change the nickname of!");
 
-    const arguments = args.slice(1).join(" ");
-
-    if (!arguments) return message.reply("Please specify a new nickname!");
-
-    if(message.member.roles.highest.position <= member.roles.highest.position) return message.channel.send("You're role is not higher than the member.")
-
-    try {
-      member.setNickname(arguments);
-      message.channel.send('I have changed the nickname!')
-    } catch (err) {
-      console.log(err);
-      message.reply(
-        "I do not have permission to set " + member.toString() + " nickname!"
-      );
+     const yess = new MessageEmbed()
+        
+        .setDescription(`:white_check_mark:  ${member.user.tag}'s Nickname has Been Moderated`)
+        .setColor("GREEN")
+        message.channel.send(yess)
     }
-  },
-};
+}
