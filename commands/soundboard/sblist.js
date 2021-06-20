55a42fa3484400a1fc96474154eb49e8e919aa7d
conghal334 <72975273+conghal334@users.noreply.github.com>
2021-06-20 23:31:48 +0700

    more new soundboard

---
+++ commands/soundboard/sblist.js
@@ -0,0 +1,29 @@
+  
+const Discord = require('discord.js')
+const { prefix } = require("../../config.json");
+
+
+module.exports = {
+    name: "sb-list",
+    aliases: ["sblist", "sb-list", "sbl"],
+    description: "Soundboard list command",
+
+    run: async (client, message, args) => {
+
+        const help = new Discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle(`Prefix: \`${prefix}\``)
+        .setAuthor('Soundboard list', message.author.displayAvatarURL())
+
+        .addFields({
+            name: `Sounboard of ${client.user.username}`,
+            value: `How to use: \n \`\`\`<prefix>sb <sound name> \`\`\``,
+        },
+        {
+            name: 'Here are the list of soundboard',
+            value: '`ahshit`, `bruh`, `uwu`',        
+        },)
+
+        message.channel.send(help)
+    }
+}
---
+++ commands/soundboard/soundboard-play.js
@@ -0,0 +1,44 @@
+const discord = require('discord.js');
+
+module.exports = {
+name: 'sb-play',
+aliases: ['soundboard-play', 'sbp', 'sb-p'],
+description: 'Play a sound insoundboard',
+run: async (client, message, args) => {
+    const sb = args.shift();
+    if(!sb) return message.channel.send("Please choose a sound!")
+if(sb === 'ahshit') {
+        channel.join().then(async connection => {
+            const dispatcher = connection.play(path.join(__dirname + '/audio/ahshit.mp3'));
+            const e = await message.react('🎙️');
+            dispatcher.on('speaking', speaking => {
+                if(!speaking) {
+            channel.leave();
+            e.remove()
+                }
+            })
+        })
+} else if(sb === 'bruh') {
+        channel.join().then(async connection => {
+		const dispatcher = connection.play(path.join(__dirname + '/audio/bruh.mp3'));
+		const e = await message.react('🎙️');
+		dispatcher.on('speaking', speaking => {
+			if(!speaking) {
+        channel.leave();
+        e.remove()
+            }
+        })
+    })
+} else if(sb === 'uwu') {
+    channel.join().then(async connection => {
+		const dispatcher = connection.play(path.join(__dirname + '/audio/uwu.mp3'));
+		const e = await message.react('🎙️');
+		dispatcher.on('speaking', speaking => {
+			if(!speaking) {
+        channel.leave();
+        e.remove()
+            }
+		});
+	})
+}
+}};
