5465a514a183e70959d2a55f19a3bef8455c4047
conghal334 <72975273+conghal334@users.noreply.github.com>
2021-06-22 00:21:40 +0700

    clown

---
+++ commands/fun/clown.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+module.exports = {
+  name: "clown",
+  async execute(client, message, args) {
+     const user =
+      message.mentions.members.first() || message.member;
+let msg = await message.channel.send('Loading...')
+      let att = new Discord.MessageAttachment(`https://api.popcatdev.repl.co/clown?image=${user.user.displayAvatarURL({ dynamic: false, format: "png"})}`, `${user.user.username}_clown.png`)
+
+      message.channel.send(att)
+      msg.delete()
+  }
+}
