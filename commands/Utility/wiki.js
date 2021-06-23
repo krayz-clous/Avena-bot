7c52bd27580401e22f4bac903b6bd424f1eeac86
conghal334 <72975273+conghal334@users.noreply.github.com>
2021-06-23 13:22:24 +0700

    fixed wiki

--- commands/Utility/wiki.js
+++ commands/Utility/wiki.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require('../../configs/config.json');
+const config = require('../../config.json');
 const fetch = require('node-fetch')
 
 module.exports = {
