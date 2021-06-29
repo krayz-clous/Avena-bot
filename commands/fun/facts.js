3c11a8e3660038306eb8b749c0d6674e8629613a
conghal334 <72975273+conghal334@users.noreply.github.com>
2021-06-29 13:51:51 +0700

    new fact command

---
+++ commands/fun/facts.js
@@ -0,0 +1,45 @@
+
+const fetch = require('node-fetch');
+const Guild = require('../../models/Guild');
+const discord = require('discord.js');
+const request = require('request-promise-native');
+const config = require("../../config.json")
+
+module.exports = {
+        name: 'facts',
+        aliases:['fact'],
+        description: 'make a fact',
+        usage: '<text>',
+
+    async run(client, message, args) {
+      const guildDB = await Guild.findOne({
+        id: message.guild.id
+      });
+    
+
+      try {
+        let text = args.slice(0).join(" ")
+        if(!text) return message.channel.send(new discord.MessageEmbed().setColor(config.color).setDescription(`:x: Try again later..`));
+        
+          if(text.length > 60) return message.channel.send(new discord.MessageEmbed().setColor(config.color).setDescription(`:x: Try again later..`));
+        let options = {
+          url: 'https://api.alexflipnote.dev/facts',
+          headers: {
+            'Authorization': config.alexflipnote
+          },
+          qs: {
+            text: args.join(' ').split('').join('')
+          },
+          encoding: null
+        };
+      
+        let response = await request(options);
+      
+        await message.channel.send({
+          files: [ response ]
+        });
+      } catch(error) {
+        this.client.emit("apiError", error, message);
+      }
+    }
+};
