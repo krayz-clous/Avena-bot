56bddb751e825e94dd07c06db62c2c261a767963
conghal334 <72975273+conghal334@users.noreply.github.com>
2021-07-06 11:45:32 +0700

    new steal emoji command

--- commands/Utility/steal.js
+++ commands/Utility/steal.js
@@ -8,18 +8,32 @@ module.exports = {
    * @param {String[]} args
    */
   run: async (client, message, args) => {
-    if(!message.member.hasPermission("MANAGE_EMOJIS")) return;
-    if(!args.length) return message.channel.send('No emoji specified');
+    if(!message.member.hasPermission("MANAGE_EMOJIS")) return message.reply("You do not have permission.")
 
-    for (const rawEmoji of args) {
-        const parsedEmoji = Util.parseEmoji(rawEmoji);
+    const emoji = args[0];
+       if (!emoji) return message.channel.send("Please provide emoji to add\n\n**Usage:** `addemoji <emoji> <name>`");
 
-        if(parsedEmoji.id) {
-            const extension = parsedEmoji.animated ? ".gif" : ".png";
-            const url = `https://cdn.discordapp.com/emojis/${parsedEmoji.id + extension}`;
-            message.guild.emojis.create(url, parsedEmoji.name)
-                .then((emoji) => message.channel.send(`Added the emoji with the link: \`${emoji.url}\``))
-        }
-    }
+      let custom = Util.parseEmoji(emoji);
+
+		const name = args[1] ? args[1].replace(/[^a-z0-9]/gi, "") : null;
+		if (!name) {
+			return message.lineReply("Please provide a name to set");
+		}
+		if (name.length < 2 || name > 32) {
+			return message.lineReply("Emoji name length should be between 2 and 32");
+		}     
+   const URL = `https://cdn.discordapp.com/emojis/${custom.id}.${custom.animated ? "gif" : "png"}`;
+   
+     message.guild.emojis
+			.create(URL, name)
+			.then(emoji => {
+				message.lineReply(`Emoji ${emoji} was successfully added`, {
+					emojiName: emoji.name
+				}).catch((err) =>{
+				    const em = new MessageEmbed()
+				    message.lineReply("Error exist")
+				    console.log(err)
+				});
+			})
   },
 };
