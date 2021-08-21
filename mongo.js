25199834b3a76438e50bb3b9512960e8bb3bfcb4
rcong <72975273+conghal334@users.noreply.github.com>
2021-08-21 09:25:14 +0700

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoPath  = "mongodb+srv://conghal:thuongvu1@avena.g9r2z.mongodb.net/test"
+const mongoPath  = "mongo_url"
 
 module.exports = async () => {
     await mongoose.connect(mongoPath, {
